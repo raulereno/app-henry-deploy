089dc95cc6ce1b63d6e8e406601dc7524579ea86
raulereno <raulereno@gmail.com>
2022-10-26 21:25:42 -0300

    agregando variabes

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const getTempes = require("./src/controllers/tempes");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     getTempes();
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
