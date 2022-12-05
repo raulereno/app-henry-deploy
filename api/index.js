6dc877ad7c2c73fc9681618a4c41edc0272c9219
Raul Ereño <88440792+raulereno@users.noreply.github.com>
2022-12-05 15:52:55 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 const getTempes = require("./src/controllers/tempes");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(process.env.PORT, () => {
     getTempes();
     console.log("%s listening at 3001"); // eslint-disable-line no-console
